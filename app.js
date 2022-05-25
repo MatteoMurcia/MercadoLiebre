d5059db657dbd83db65fb18195325925c8b15e04
Matteo Murcia <matteo@cbloom.com>
2022-05-25 09:24:43 -0500

    port

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const PORT = process.eventNames.PORT || 3000;
 
 const app = express();
 const publicFolderPath = path.resolve(__dirname,'./public');
@@ -17,6 +18,6 @@ app.get('/register',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/register.html'));
 })
 
-app.listen(3030,()=>{
+app.listen(PORT,()=>{
     console.log('running..')
 });

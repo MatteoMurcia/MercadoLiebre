bf176e443fbd93aed2230616e0febf2e04f12108
Matteo Murcia <matteo@cbloom.com>
2022-05-25 08:34:52 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const publicFolderPath = path.resolve(__dirname,'./public');
+app.use(express.static(publicFolderPath));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/home.html'));
+})
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'));
+})
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'));
+})
+
+app.listen(3030,()=>{
+    console.log('running..')
+});
